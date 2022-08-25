236bf93845a83d2fdf1ffb53de0c470f167725eb
Abuzeid <abuzeid1999@gmail.com>
2022-08-25 17:07:16 +0200

    2.1.1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const parent = document.querySelector(".header");
 const child = document.querySelector(".header > .header-content");
 const FirstOffsetBottom =
-  parent.offsetHeight - child.clientHeight - child.offsetHeight - 1;
+  parent.offsetHeight - child.offsetTop - child.offsetHeight;
 
 document.addEventListener("scroll", changeOpacity);
 function changeOpacity() {
-  let offsetBottom =
-    parent.offsetHeight - child.offsetHeight - child.offsetTop - 1;
+  let offsetBottom = parent.offsetHeight - child.offsetTop - child.offsetHeight;
+
   child.style.opacity = offsetBottom / FirstOffsetBottom;
 }
