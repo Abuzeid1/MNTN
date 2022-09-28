94f6ff1f91febbd13a0a45cd39a1bc61ad6d56e9
Abuzeid <abuzeid1999@gmail.com>
2022-09-28 12:26:36 +0200

    Fix opactiy changing image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-const parent = document.querySelector(".header");
-const child = document.querySelector(".header > .header-content");
-const FirstOffsetBottom =
-  parent.offsetHeight - child.offsetTop - child.offsetHeight;
+const parent = document.querySelector("header.header");
+const child = document.querySelector("header.header > .header-content.section");
 
+const height = parent.offsetHeight - child.offsetHeight;
+const FirstOffsetTop = child.offsetTop;
 document.addEventListener("scroll", changeOpacity);
 function changeOpacity() {
-  let offsetBottom = parent.offsetHeight - child.offsetTop - child.offsetHeight;
-
-  child.style.opacity = offsetBottom / FirstOffsetBottom;
+  child.style.opacity = (height - child.offsetTop) / (height - FirstOffsetTop);
 }
